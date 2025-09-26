935bee03cabdd1f260013257858b898c1837b5f3
GabrielLimaSC <gabrielsiqueira311@gmail.com>
2025-09-25 21:03:51 -0300

    add mongoose for MongoDB integration and implement CRUD operations for films

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://gabrielsiqueira311_db_user:Binho2010@starwars-api.lonjzvo.mongodb.net/?retryWrites=true&w=majority&appName=StarWars-API');
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+const Film = mongoose.model('Film', {
+    title: String,
+    description: String,
+    image_url: String,
+    trailer_url: String
+});
+
+
+app.get('/', async (req, res) => {
+    const films = await Film.find();
+    return res.send(films);
+});
+
+app.delete("/:id", async(req, res) => {
+    const film = await Film.findByIdAndDelete(req.params.id);
+    return res.send(film);
+})
+
+app.put("/:id", async(req, res) => {
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+    }, {new: true});
+    return res.send(film);
+});
+
+app.post('/', async (req, res) => {
+    const film = new Film({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+    });
+
+    film.save().then(() => res.send('Film created'));
+    return res.send(film);
+})
+
+app.listen(port, () => { console.log('App running')});
